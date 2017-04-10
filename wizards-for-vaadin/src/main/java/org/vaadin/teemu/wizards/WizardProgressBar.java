2249f23141a9b7960143eb02c2bc43f72bfb7a34
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2017-04-10 16:06:00 -0500

    Display the amount of specified titles at once in the progress bar.

--- wizards-for-vaadin/src/main/java/org/vaadin/teemu/wizards/WizardProgressBar.java
+++ wizards-for-vaadin/src/main/java/org/vaadin/teemu/wizards/WizardProgressBar.java
@@ -1,19 +1,17 @@
 package org.vaadin.teemu.wizards;
 
-import java.util.List;
-
-import org.vaadin.teemu.wizards.event.WizardCancelledEvent;
-import org.vaadin.teemu.wizards.event.WizardCompletedEvent;
-import org.vaadin.teemu.wizards.event.WizardProgressListener;
-import org.vaadin.teemu.wizards.event.WizardStepActivationEvent;
-import org.vaadin.teemu.wizards.event.WizardStepSetChangedEvent;
-
 import com.vaadin.annotations.StyleSheet;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.ProgressBar;
 import com.vaadin.ui.VerticalLayout;
+import java.util.List;
+import org.vaadin.teemu.wizards.event.WizardCancelledEvent;
+import org.vaadin.teemu.wizards.event.WizardCompletedEvent;
+import org.vaadin.teemu.wizards.event.WizardProgressListener;
+import org.vaadin.teemu.wizards.event.WizardStepActivationEvent;
+import org.vaadin.teemu.wizards.event.WizardStepSetChangedEvent;
 
 /**
  * Displays a progress bar for a {@link Wizard}.
@@ -27,6 +25,8 @@ public class WizardProgressBar extends CustomComponent implements
     private final ProgressBar progressBar = new ProgressBar();
     private final HorizontalLayout stepCaptions = new HorizontalLayout();
     private int activeStepIndex;
+    //Amount of steps displayed at a time in the header.
+    private int maxStepsDisplayed = -1;//Show all
 
     public WizardProgressBar(Wizard wizard) {
         setStyleName("wizard-progress-bar");
@@ -54,10 +54,16 @@ public class WizardProgressBar extends CustomComponent implements
     private void updateStepCaptions() {
         stepCaptions.removeAllComponents();
         int index = 1;
+        //Just show 3 at a time
         for (WizardStep step : wizard.getSteps()) {
-            Label label = createCaptionLabel(index, step);
-            stepCaptions.addComponent(label);
+            if (index > activeStepIndex) {
+                Label label = createCaptionLabel(index, step);
+                stepCaptions.addComponent(label);
+            }
             index++;
+            if (maxStepsDisplayed > 0 && index > activeStepIndex + maxStepsDisplayed) {
+                break;
+            }
         }
     }
 
@@ -108,5 +114,28 @@ public class WizardProgressBar extends CustomComponent implements
     @Override
     public void wizardCancelled(WizardCancelledEvent event) {
         // NOP, no need to react to cancellation
+    }
+
+    /**
+     * Get amount of steps to display on the header at a time. A value of -1
+     * means all will be displayed.
+     *
+     * @return the maxStepsDisplayed
+     */
+    public int getMaxStepsDisplayed() {
+        return maxStepsDisplayed;
+    }
+
+    /**
+     * Set the maximum amount of steps to display on the header at a time to
+     * avoid them being overlapping.
+     *
+     * A value of -1 means all will be displayed.
+     *
+     * @param maxStepsDisplayed the maxStepsDisplayed to set
+     */
+    public void setMaxStepsDisplayed(int maxStepsDisplayed) {
+        this.maxStepsDisplayed = maxStepsDisplayed;
+        updateStepCaptions();
     }
 }
