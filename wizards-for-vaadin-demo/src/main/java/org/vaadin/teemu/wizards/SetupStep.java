2b43bd491af3d750bf5d739521e8118f7b39fdf8
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2017-04-29 12:40:11 -0500

    Add @Override annotations.

--- wizards-for-vaadin-demo/src/main/java/org/vaadin/teemu/wizards/SetupStep.java
+++ wizards-for-vaadin-demo/src/main/java/org/vaadin/teemu/wizards/SetupStep.java
@@ -7,10 +7,12 @@ import com.vaadin.ui.VerticalLayout;
 
 public class SetupStep implements WizardStep {
 
+    @Override
     public String getCaption() {
         return "Initialize the Wizard";
     }
 
+    @Override
     public Component getContent() {
         VerticalLayout content = new VerticalLayout();
         content.setSizeFull();
@@ -25,19 +27,20 @@ public class SetupStep implements WizardStep {
     private Label getText() {
         return new Label(
                 "<h2>Initialize the Wizard</h2><p>Create an instance of the <code>Wizard</code> by calling the default constructor.</p>"
-                        + "<pre>Wizard myWizard = new Wizard()</pre><p>After instantiation you can add some steps to the wizard by "
-                        + "calling the <code>addStep</code> method. The method accepts instances of the <code>WizardStep</code> interface "
-                        + "so you need to implement that in order to display your steps."
-                        + "<pre>myWizard.addStep(new FirstStep());\nmyWizard.addStep(new SecondStep());\nmyWizard.addStep(new ThirdStep());\nmyWizard.addStep(new FourthStep());</pre>",
+                + "<pre>Wizard myWizard = new Wizard()</pre><p>After instantiation you can add some steps to the wizard by "
+                + "calling the <code>addStep</code> method. The method accepts instances of the <code>WizardStep</code> interface "
+                + "so you need to implement that in order to display your steps."
+                + "<pre>myWizard.addStep(new FirstStep());\nmyWizard.addStep(new SecondStep());\nmyWizard.addStep(new ThirdStep());\nmyWizard.addStep(new FourthStep());</pre>",
                 ContentMode.HTML);
     }
 
+    @Override
     public boolean onAdvance() {
         return true;
     }
 
+    @Override
     public boolean onBack() {
         return true;
     }
-
 }
